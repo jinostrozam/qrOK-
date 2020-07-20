3a797f884a5c891fa967aa034436b133c1cff458
Jaime Inostroza <bladimirosky@gmail.com>
2020-07-19 23:06:18 -0400

    Tutorial 12 - Register With Email and Password

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,28 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:qrok/services/auth.dart';
+import 'package:email_validator/email_validator.dart';
 
-class SignIn extends StatefulWidget {
+class Register extends StatefulWidget {
   // Constructor for allowing parameters in ToggleView Function in register()
   final Function toggleView;
-  SignIn({this.toggleView});
+  Register({this.toggleView});
 
   @override
-  _SignInState createState() => _SignInState();
+  _RegisterState createState() => _RegisterState();
 }
 
-class _SignInState extends State<SignIn> {
+class _RegisterState extends State<Register> {
+  @override
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // Text field state
   String email = '';
   String password = '';
+  String error = '';
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 0.0,
-        title: Text('Ingresa a qrOK!'),
+        title: Text('Registrate en qrOK!'),
         // centerTitle: true,
         actions: <Widget>[
           FlatButton.icon(
@@ -32,60 +35,68 @@ class _SignInState extends State<SignIn> {
             icon: Icon(
               Icons.person,
             ),
-            label: Text('Registro'),
+            label: Text('Ingreso'),
             textColor: Colors.white,
           ),
         ],
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
-              SizedBox(height: 20.0),
+              SizedBox(height: 10.0),
               TextFormField(
+                // validator: (val) => val.isEmpty ? 'Ingresa un correo' : null,
+                validator: (val) =>
+                    EmailValidator.validate(email) ? null : "Correo Invalido",
+                keyboardType: TextInputType.emailAddress,
+                textInputAction: TextInputAction.next,
                 decoration: InputDecoration(labelText: 'Correo Electronico'),
                 onChanged: (val) {
                   setState(() => email = val);
                 },
               ),
-              SizedBox(height: 20.0),
+              SizedBox(height: 10.0),
               TextFormField(
+                validator: (val) => val.length < 6
+                    ? 'Ingresa una clave con mas de 6 digitos'
+                    : null,
                 decoration: InputDecoration(labelText: 'Contraseña'),
+                textInputAction: TextInputAction.next,
                 obscureText: true,
                 onChanged: (val) {
                   setState(() => password = val);
                 },
               ),
-              SizedBox(height: 20.0),
+              SizedBox(height: 10.0),
               RaisedButton(
                 color: Colors.blue,
                 child: Text(
-                  'Ingresar',
+                  'Registrarse',
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async {
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()) {}
+                  // print(email);
+                  // print(password);
+                  dynamic result =
+                      await _auth.registerWithEmailAndPassword(email, password);
+                  if (result == null) {
+                    setState(
+                        () => error = 'Correo Invalido. Intente Nuevamente');
+                  }
                 },
-              )
+              ),
+              SizedBox(height: 15.0),
+              Text(
+                error,
+                style: TextStyle(color: Colors.red, fontSize: 12.0),
+              ),
             ],
           ),
         ),
-
-        // child: RaisedButton(
-        //   child: Text('Ingreso anonimo'),
-        //   onPressed: () async {
-        //     dynamic result = await _auth.signInAnon();
-        //     if (result == null) {
-        //       print('Problema al ingresar');
-        //     } else {
-        //       print('Ingreso exitoso');
-        //       print(result.uid);
-        //     }
-        //   },
-        // ),
       ),
     );
   }
