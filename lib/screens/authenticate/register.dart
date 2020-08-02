36ce2363fae1c8da838d5faec79cd4d44ee3ecf1
Jaime Inostroza <bladimirosky@gmail.com>
2020-08-01 22:43:13 -0400

    Tutorail 15 - Loading Widget

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qrok/services/auth.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:qrok/shared/loading.dart';
 
 class Register extends StatefulWidget {
   // Constructor for allowing parameters in ToggleView Function in register()
@@ -16,6 +17,9 @@ class _RegisterState extends State<Register> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
 
+  // Loading Widget
+  bool loading = false;
+
   // Text field state
   String email = '';
   String password = '';
@@ -23,83 +27,92 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        title: Text('Registrate en qrOK!'),
-        // centerTitle: true,
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(
-              Icons.person,
-            ),
-            label: Text('Ya tengo \n  cuenta'),
-            textColor: Colors.white,
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 10.0),
-              TextFormField(
-                // validator: (val) => val.isEmpty ? 'Ingresa un correo' : null,
-                validator: (val) =>
-                    EmailValidator.validate(email) ? null : "Correo Invalido",
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(labelText: 'Correo Electronico'),
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 10.0),
-              TextFormField(
-                validator: (val) => val.length < 6
-                    ? 'Ingresa una clave con mas de 6 digitos'
-                    : null,
-                decoration: InputDecoration(labelText: 'Contraseña'),
-                textInputAction: TextInputAction.next,
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 10.0),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
-                  'Registrarse',
-                  style: TextStyle(color: Colors.white),
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              elevation: 0.0,
+              title: Text('Registrate en qrOK!'),
+              // centerTitle: true,
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                  icon: Icon(
+                    Icons.person,
+                  ),
+                  label: Text('Ya tengo \n  cuenta'),
+                  textColor: Colors.white,
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    // print(email);
-                    // print(password);
-                    dynamic result = await _auth.registerWithEmailAndPassword(
-                        email, password);
-                    if (result == null) {
-                      setState(() =>
-                          error = 'Error de registro. Intente nuevamente');
-                    }
-                  }
-                },
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      // validator: (val) => val.isEmpty ? 'Ingresa un correo' : null,
+                      validator: (val) => EmailValidator.validate(email)
+                          ? null
+                          : "Correo Invalido",
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.next,
+                      decoration:
+                          InputDecoration(labelText: 'Correo Electronico'),
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      validator: (val) => val.length < 6
+                          ? 'Ingresa una clave con mas de 6 digitos'
+                          : null,
+                      decoration: InputDecoration(labelText: 'Contraseña'),
+                      textInputAction: TextInputAction.next,
+                      obscureText: true,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 10.0),
+                    RaisedButton(
+                      color: Colors.blue,
+                      child: Text(
+                        'Registrarse',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() {
+                            loading = true;
+                          });
+                          // print(email);
+                          // print(password);
+                          dynamic result = await _auth
+                              .registerWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = 'Error de registro. Intente nuevamente';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 15.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 12.0),
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(height: 15.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 12.0),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:qrok/services/auth.dart';
 import 'package:email_validator/email_validator.dart';
+import 'package:qrok/shared/loading.dart';
 
 class SignIn extends StatefulWidget {
   // Constructor for allowing parameters in ToggleView Function in register()
@@ -16,6 +17,9 @@ class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
 
+  // Loading Widget
+  bool loading = false;
+
   // Text field state
   String email = '';
   String password = '';
@@ -23,96 +27,105 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        title: Text('Ingresa a qrOK!'),
-        // centerTitle: true,
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () {
-              widget.toggleView();
-            },
-            icon: Icon(
-              Icons.person,
-            ),
-            label: Text('No tengo \n  cuenta'),
-            textColor: Colors.white,
-          ),
-        ],
-      ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
-
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 10.0),
-              TextFormField(
-                validator: (val) =>
-                    EmailValidator.validate(email) ? null : "Correo Invalido",
-                keyboardType: TextInputType.emailAddress,
-                textInputAction: TextInputAction.next,
-                decoration: InputDecoration(labelText: 'Correo Electronico'),
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
-              ),
-              SizedBox(height: 10.0),
-              TextFormField(
-                validator: (val) => val.length < 6
-                    ? 'Ingresa una clave con mas de 6 digitos'
-                    : null,
-                decoration: InputDecoration(labelText: 'Contraseña'),
-                obscureText: true,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(height: 10.0),
-              RaisedButton(
-                color: Colors.blue,
-                child: Text(
-                  'Ingresar',
-                  style: TextStyle(color: Colors.white),
+    return loading
+        ? Loading()
+        : Scaffold(
+            appBar: AppBar(
+              elevation: 0.0,
+              title: Text('Ingresa a qrOK!'),
+              // centerTitle: true,
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    widget.toggleView();
+                  },
+                  icon: Icon(
+                    Icons.person,
+                  ),
+                  label: Text('No tengo \n  cuenta'),
+                  textColor: Colors.white,
                 ),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    print('valid');
-                    // print(email);
-                    // print(password);
-                    dynamic result =
-                        await _auth.signInWithEmailAndPassword(email, password);
-                    if (result == null) {
-                      setState(() =>
-                          error = 'Error al Ingresar. Intente nuevamente');
-                    }
-                  }
-                },
-              ),
-              SizedBox(height: 15.0),
-              Text(
-                error,
-                style: TextStyle(color: Colors.red, fontSize: 12.0),
-              ),
-            ],
-          ),
-        ),
+              ],
+            ),
+            body: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
 
-        // child: RaisedButton(
-        //   child: Text('Ingreso anonimo'),
-        //   onPressed: () async {
-        //     dynamic result = await _auth.signInAnon();
-        //     if (result == null) {
-        //       print('Problema al ingresar');
-        //     } else {
-        //       print('Ingreso exitoso');
-        //       print(result.uid);
-        //     }
-        //   },
-        // ),
-      ),
-    );
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      validator: (val) => EmailValidator.validate(email)
+                          ? null
+                          : "Correo Invalido",
+                      keyboardType: TextInputType.emailAddress,
+                      textInputAction: TextInputAction.next,
+                      decoration:
+                          InputDecoration(labelText: 'Correo Electronico'),
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(height: 10.0),
+                    TextFormField(
+                      validator: (val) => val.length < 6
+                          ? 'Ingresa una clave con mas de 6 digitos'
+                          : null,
+                      decoration: InputDecoration(labelText: 'Contraseña'),
+                      obscureText: true,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(height: 10.0),
+                    RaisedButton(
+                      color: Colors.blue,
+                      child: Text(
+                        'Ingresar',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() {
+                            loading = true;
+                          });
+                          print('valid');
+                          // print(email);
+                          // print(password);
+                          dynamic result = await _auth
+                              .signInWithEmailAndPassword(email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = 'Error al Ingresar. Intente nuevamente';
+                              loading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    SizedBox(height: 15.0),
+                    Text(
+                      error,
+                      style: TextStyle(color: Colors.red, fontSize: 12.0),
+                    ),
+                  ],
+                ),
+              ),
+
+              // child: RaisedButton(
+              //   child: Text('Ingreso anonimo'),
+              //   onPressed: () async {
+              //     dynamic result = await _auth.signInAnon();
+              //     if (result == null) {
+              //       print('Problema al ingresar');
+              //     } else {
+              //       print('Ingreso exitoso');
+              //       print(result.uid);
+              //     }
+              //   },
+              // ),
+            ),
+          );
   }
 }
