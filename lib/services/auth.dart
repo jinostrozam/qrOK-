85b965aea103b56e86b781e89c06b8652744f733
Jaime Inostroza <bladimirosky@gmail.com>
2020-08-02 19:26:52 -0400

    Tutorail 17 - Creando Registros de Prueba en Firestore

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,6 +1,7 @@
 // import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:qrok/models/users.dart';
+import 'package:qrok/services/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -49,6 +50,11 @@ class AuthService {
       AuthResult result = await _auth.createUserWithEmailAndPassword(
           email: email, password: password);
       FirebaseUser user = result.user;
+
+      // create a new docuemnt for the user with the uid
+      await DatabaseService(uid: user.uid)
+          .updateUserData('0', 'new crew member', 100);
+
       return _userFromFirebaseUser(user);
     } catch (e) {
       print(e.toString());
